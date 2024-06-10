959a3d00e0e6cebef2c5884fa8018edfb7ad04e5
miyanao <k023g0050@g.neec.ac.jp>
2024-06-10 12:12:55 +0900

    EN1_引っ張りジャンプ09_アニメーション02

--- Assets/ItemScript.cs
+++ Assets/ItemScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ItemScript : MonoBehaviour
 {
+	private Animator animator;
+
 	// Start is called before the first frame update
 	void Start()
 	{
-		
+		animator = GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
@@ -24,7 +26,8 @@ public class ItemScript : MonoBehaviour
 	private void OnTriggerEnter(Collider other)
 	{
 		// ê⁄êGÇµÇΩèuä‘Ç…åƒÇŒÇÍÇÈ
-		DestroySelf();
+		//DestroySelf();
+		animator.SetTrigger("Get");
 	}
 
 	private void OnTriggerStay(Collider other)
