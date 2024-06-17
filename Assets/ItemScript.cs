d23c8f1b4edda8cd2a0177c64f09fbab2167ffa1
miyanao <k023g0050@g.neec.ac.jp>
2024-06-17 10:46:49 +0900

    EN1_引っ張りジャンプ12_サウンド(SE)

--- Assets/ItemScript.cs
+++ Assets/ItemScript.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class ItemScript : MonoBehaviour
 {
 	private Animator animator;
+	private AudioSource audioSource;
 
 	// Start is called before the first frame update
 	void Start()
 	{
 		animator = GetComponent<Animator>();
+		audioSource = GetComponent<AudioSource>();
 	}
 
 	// Update is called once per frame
@@ -27,6 +29,7 @@ public class ItemScript : MonoBehaviour
 	{
 		// ê⁄êGÇµÇΩèuä‘Ç…åƒÇŒÇÍÇÈ
 		animator.SetTrigger("Get");
+		audioSource.Play();
 	}
 
 	private void OnTriggerStay(Collider other)
